448e2ec38375b3973463e2f74ddaa15bbc6539c2
김준성 <anima94@naver.com>
2020-03-06 13:04:45 +0900

    feat) intent kotlin

--- app/src/main/java/junseong/kotlin/firebase/MainActivity.kt
+++ app/src/main/java/junseong/kotlin/firebase/MainActivity.kt
@@ -1,12 +1,19 @@
 package junseong.kotlin.firebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login.setOnClickListener {
+            val intent= Intent(this,LoginActivity::class.java)
+            startActivity(intent);
+        }
     }
 }

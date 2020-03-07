b0240d3a0b6af8065c69014c10e966bb40a4d489
김준성 <anima94@naver.com>
2020-03-07 14:19:20 +0900

    feat) firebase email auth

--- app/src/main/java/junseong/kotlin/firebase/LoginActivity.kt
+++ app/src/main/java/junseong/kotlin/firebase/LoginActivity.kt
@@ -1,12 +1,39 @@
 package junseong.kotlin.firebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-
+    private lateinit var auth: FirebaseAuth// ...
+    private val TAG= LoginActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+
+        login_button.setOnClickListener {
+            val email=login_email.text.toString()
+            val password=login_pwd.text.toString()
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val intent= Intent(this,ChatlistActivity::class.java)
+                        intent.flags= Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+
+                    }
+
+                    // ...
+                }
+        }
     }
 }
--- app/src/main/java/junseong/kotlin/firebase/MainActivity.kt
+++ app/src/main/java/junseong/kotlin/firebase/MainActivity.kt
@@ -1,15 +1,40 @@
 package junseong.kotlin.firebase
 
 import android.content.Intent
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth// ...
+    private val Tag:String =MainActivity::class.java.simpleName
+// Initialize Firebase Auth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+
+        join.setOnClickListener {
+            val email=email_area.text.toString()
+            val password=password_area.text.toString()
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(Tag,"성공")
+                        val intent=Intent(this,ChatlistActivity::class.java)
+                        intent.flags=Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                    } else {
+                        Log.d(Tag,"실패")
+                    }
+
+                    // ...
+                }
+        }
 
         login.setOnClickListener {
             val intent= Intent(this,LoginActivity::class.java)
